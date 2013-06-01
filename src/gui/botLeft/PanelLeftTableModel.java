4d486f50ca4b03ba79052184d1cff72a333ba02c
dobin <dobin@unreal>
2013-06-01 13:04:43 +0200

    - license update

--- src/gui/botLeft/PanelLeftTableModel.java
+++ src/gui/botLeft/PanelLeftTableModel.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 DobinRutishauser@broken.ch
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package gui.botLeft;
 
 import attacks.AttackMain;
@@ -6,10 +22,8 @@ import gui.session.SessionManager;
 import java.util.LinkedList;
 import java.util.Observable;
 import java.util.Observer;
-import javax.swing.JCheckBox;
 import javax.swing.table.DefaultTableModel;
 import model.SentinelHttpParam;
-import model.SentinelHttpMessage;
 import model.SentinelHttpMessageAtk;
 import model.SentinelHttpMessageOrig;
 import util.BurpCallbacks;
--- src/gui/networking/Networker.java
+++ src/gui/networking/Networker.java
@@ -1,17 +1,33 @@
+/*
+ * Copyright (C) 2013 DobinRutishauser@broken.ch
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package gui.networking;
 
 import gui.botLeft.PanelLeftUi;
 import java.util.LinkedList;
-import model.SentinelHttpMessage;
 import model.SentinelHttpMessageOrig;
 import model.SentinelHttpParam;
+import util.BurpCallbacks;
 
 /**
  *
  * @author dobin
  */
 public class Networker {
-    private static Networker myself;
+    private static Networker myself = null;
     public static Networker getInstance() {
         if (myself == null) {
             myself = new Networker();
@@ -29,6 +45,7 @@ public class Networker {
 
     public void init() {
         worker = new NetworkerWorker();
+        BurpCallbacks.getInstance().print("ATTACK! PPRE");
         worker.execute();
     }
 
